7fd140330afaac4860abba5e90150586239631b9
TrungTran <tranletrung3004@gmail.com>
2025-10-21 20:35:14 +0700

    "feature: khởi tạo biến của class Tour"

--- src/main/java/OOP/Nhom15/QuanLiTourDuLich/model/Tour.java
+++ src/main/java/OOP/Nhom15/QuanLiTourDuLich/model/Tour.java
@@ -1,4 +1,16 @@
 package OOP.Nhom15.QuanLiTourDuLich.model;
 
+import java.time.LocalDate;
+
 public class Tour {
+    //Bắt đầu khởi tạo biến nè
+    private String maTour;
+    private String tenTour;
+    private String diaDiem;
+    private double giaTour;
+    private int soNgay;
+    private String phuongTien;
+    private String huongDanVien;
+    private LocalDate ngayBatDau;
+    private LocalDate ngayKetThuc;
 }
